8a9df0c1f2762417823649e374f0311b19cbd340
Jaikumar <kanishjaikumar06@gmail.com>
2025-12-18 15:47:39 +0530

    fsModule Async function

---
+++ NodeJs/fsAsyncModule.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
+const folderPath = path.join(__dirname,"async");
+const filePath = path.join(folderPath, "new.txt");
+
+fs.mkdir(folderPath,{recursive:true},(error)=>{
+    if(error){
+        console.error(error);
+    }else{
+        console.log("Folder created successfully");
+        fs.writeFile(filePath,"Hello,This is a new file",(error)=>{
+    if(error){
+        console.error(error);
+    }else{
+        console.log("File created successfully");
+        fs.readFile(filePath,"utf-8",(error,data)=>{
+      if(error){
+        console.error(error);
+    }else{
+        console.log(data);
+    }
+})
+    }
+});
+    }
+});
